e466eaf89809eafda7028f174d36d7114dc28205
Jake Hill <Jake_hill666@hotmail.co.uk>
2017-03-02 12:03:46 +0100

    modify html, css and js (bad commit message, forgotten what modifications were made, must commit more frequently)

--- portfolio/js/scripts.js
+++ portfolio/js/scripts.js
@@ -1,12 +1,3 @@
-// Facebook
-(function(d, s, id) {
-    var js, fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) return;
-    js = d.createElement(s); js.id = id;
-    js.src = "//connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v2.8";
-    fjs.parentNode.insertBefore(js, fjs);
-  }(document, 'script', 'facebook-jssdk'));
-
 // Twitter
 !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');
 
@@ -116,11 +107,14 @@ $(document).ready(function(){
   // Toggle title of work on hover
   $(".work-image").mouseenter(function(){
     $(this).siblings(".info").show();
-  })
+  });
   $(".work-image").mouseleave(function(){
     $(this).siblings(".info").hide();
-  })
+  });
+
+
 });
+
 
 function initMap() {
   var myLatLng = {lat: 63.4241837, lng: 10.3832649}
